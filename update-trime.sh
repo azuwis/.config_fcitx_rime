c547e3ec5cb93fb57decafa18c292a282c3536e8
Zhong Jianxin <azuwis@gmail.com>
2015-10-12 11:37:20 +0800

    Add update-trime.sh
    for update trime on android

---
+++ update-trime.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu
+
+cd "$(dirname "$(readlink -f "$0")")"
+
+for i in flypy_yinxing*.yaml /usr/share/rime-data/pinyin_simp.*.yaml /usr/share/rime-data/default.yaml
+do
+    adb push "$i" /sdcard/rime/
+done
+
+adb shell sed -i -e '/- schema:/d' -e '/^schema_list:/a \  - schema: flypy_yinxing' /sdcard/rime/default.yaml
