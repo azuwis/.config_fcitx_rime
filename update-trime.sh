174ad0e0f8b98cb39af869bc7e2545f6a08e010f
Zhong Jianxin <azuwis@gmail.com>
2015-12-12 21:18:39 +0800

    trime: Generate default.yaml on pc
    sed is not available on stock android

--- update-trime.sh
+++ update-trime.sh
@@ -3,9 +3,12 @@ set -eu
 
 cd "$(dirname "$(readlink -f "$0")")"
 
-for i in trime.custom.yaml flypy_yinxing*.yaml /usr/share/rime-data/pinyin_simp.*.yaml /usr/share/rime-data/default.yaml
+for i in trime.custom.yaml flypy_yinxing*.yaml /usr/share/rime-data/pinyin_simp.*.yaml
 do
     adb push "$i" /sdcard/rime/
 done
 
-adb shell sed -i -e '/- schema:/d' -e '/^schema_list:/a \  - schema: flypy_yinxing' /sdcard/rime/default.yaml
+temp_file="$(mktemp)"
+sed -e '/- schema:/d' -e '/^schema_list:/a \  - schema: flypy_yinxing' /usr/share/rime-data/default.yaml > "$temp_file"
+adb push "$temp_file" /sdcard/rime/default.yaml
+rm "$temp_file"
