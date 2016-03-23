cf0840d5d5b8ca6d61b8bb0dff7c19ffb9084d71
Zhong Jianxin <azuwis@gmail.com>
2016-03-23 11:40:41 +0800

    Remove xxm from update-trime.sh

--- update-trime.sh
+++ update-trime.sh
@@ -3,13 +3,13 @@ set -eu
 
 cd "$(dirname "$(readlink -f "$0")")"
 
-for i in trime.custom.yaml flypy.*.yaml xxm.*.yaml /usr/share/rime-data/pinyin_simp.*.yaml
+for i in trime.custom.yaml flypy.*.yaml /usr/share/rime-data/pinyin_simp.*.yaml
 do
     adb push "$i" /sdcard/rime/
 done
 
 temp_file="$(mktemp)"
-sed -e '/- schema:/d' -e '/^schema_list:/a \  - schema: flypy' -e '/^schema_list:/a \  - schema: xxm' /usr/share/rime-data/default.yaml > "$temp_file"
+sed -e '/- schema:/d' -e '/^schema_list:/a \  - schema: flypy' /usr/share/rime-data/default.yaml > "$temp_file"
 adb push "$temp_file" /sdcard/rime/default.yaml
 rm "$temp_file"
 
